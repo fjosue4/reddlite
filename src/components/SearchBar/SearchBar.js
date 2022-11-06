467f42ec7d3d1839363aeed862db30f314884f3b
Franklin Méndez <franklinjosue.net@gmail.com>
2022-11-06 14:57:10 -0600

    SearchResults show and hide states

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,13 +1,25 @@
-import React, { useState, useEffect } from 'react'
-import { Icon } from '@iconify/react'
-import SearchedResult from '../SearchedResult/SearchedResult'
-import './SearchBar.css'
-
-import { useSelector, useDispatch } from 'react-redux'
-import { getSearchedData } from '../../store/search/searchSlice'
+import React, { useState, useEffect } from 'react';
+import { Icon } from '@iconify/react';
+import SearchedResult from '../SearchedResult/SearchedResult';
+import './SearchBar.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { getSearchedData } from '../../store/search/searchSlice';
 
 function SearchBar (props) {
-  const [searchTerm, setSearchTerm] = useState('')
+  const [searchTerm, setSearchTerm] = useState('');
+  const [inputFocus, setInputFocus] = useState(false);
+
+  const showSearchResults = () => {
+    setInputFocus(true);
+  };
+
+  const hideSearchResults = () => {
+    searchTerm.length === 0 ? setInputFocus(false) : setInputFocus(true);
+  };
+
+  useEffect(() => {
+    hideSearchResults()
+  }, [searchTerm]);
 
   const searchedContent = useSelector(state => state.search)
   const dispatch = useDispatch()
@@ -24,6 +36,7 @@ function SearchBar (props) {
       <Icon icon='ant-design:search-outlined' className='search-icon' />
       <input
         onChange={handleChange}
+        onClick={showSearchResults}
         value={searchTerm}
         type='search'
         placeholder='Search ReddLite'
@@ -31,7 +44,7 @@ function SearchBar (props) {
       ></input>
       {/* <button onClick={handleSearch}>Search</button> */}
       </div>
-      <SearchedResult />
+      {inputFocus === true ? <SearchedResult /> : null}
     </div>
   )
 }
