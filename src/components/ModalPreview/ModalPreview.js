e7622cd853a45892c832b6e591523e0a6d93f3f8
Vikash Kumar <vikashraj041013@gmail.com>
2022-11-22 21:21:09 +0530

    feat: extract data from data

--- src/components/ModalPreview/ModalPreview.js
+++ src/components/ModalPreview/ModalPreview.js
@@ -7,13 +7,39 @@ import ReactImageFallback from 'react-image-fallback'
 
 const ModalPreview = () => {
   const dispatch = useDispatch()
-  const { info, data } = useSelector(state => state.modal)
+  const { info, data } = useSelector((state) => state.modal)
+
+  const categorical_data = data
+    .filter((d) => d.kind === 't3')
+    .map((item) => {
+      // selftext
+      // thumbnail
+      // url_overridden_by_dest
+      // is_video then use media.fallback_url or scrubber_media_url thumbnail
+
+      // TYPE 1: selftext or url_overidden_by_dest
+      // TYPE 2. is_video or thumbnail
+
+      const updated_item = {
+        title: item?.data.title,
+        text: item?.data?.selftext_html, // or use selftext ( which is in markdown )
+        thumbnail: item?.data?.thumbnail,
+        url: item?.data?.url_overridden_by_dest,
+        isVideo: item?.data?.is_video,
+        videoLink: item?.data?.media?.scrubber_media_url,
+      }
+
+      return updated_item
+    })
+  console.log('Categorical data', categorical_data)
+  console.log(data)
+
   console.log('Data related to the search result', data)
   const modalHandler = () => {
     dispatch(changeModal.toggleModal())
   }
 
-  const numFormatter = num => {
+  const numFormatter = (num) => {
     if (num > 999 && num < 1000000) {
       return (num / 1000).toFixed(1) + 'K'
     } else if (num > 1000000) {
@@ -24,31 +50,31 @@ const ModalPreview = () => {
   }
 
   return (
-    <div class='modal'>
-      <div class='modal-content'>
-        <div className='modal-head'>
-          <span class='close' onClick={modalHandler}>
-            <Icon icon='eva:arrow-ios-back-fill' />
+    <div class="modal">
+      <div class="modal-content">
+        <div className="modal-head">
+          <span class="close" onClick={modalHandler}>
+            <Icon icon="eva:arrow-ios-back-fill" />
           </span>
-          <p className='url-preview'>
+          <p className="url-preview">
             /
             {info?.kind === 't2'
               ? `u/${info?.data.name}`
               : info?.data.display_name_prefixed}
           </p>
         </div>
-        <div className='modal-body'>
-          <div className='name-and-photo'>
+        <div className="modal-body">
+          <div className="name-and-photo">
             <ReactImageFallback
-              className='r-main-picture'
+              className="r-main-picture"
               src={
                 info?.data.snoovatar_img?.trim() === ''
                   ? info?.data.icon_img
                   : info?.data.snoovatar_img
               }
-              fallbackImage='https://www.redditstatic.com/avatars/defaults/v2/avatar_default_4.png'
+              fallbackImage="https://www.redditstatic.com/avatars/defaults/v2/avatar_default_4.png"
             />
-            <div className='karma-or-members'>
+            <div className="karma-or-members">
               <h2>
                 {info?.kind === 't2'
                   ? info?.data.name
@@ -57,18 +83,29 @@ const ModalPreview = () => {
               <span>
                 {info?.kind === 't2'
                   ? numFormatter(
-                      info?.data.link_karma + info?.data.comment_karma
+                      info?.data.link_karma + info?.data.comment_karma,
                     ) + ' Karma'
                   : numFormatter(info?.data.subscribers) + ' Members'}
               </span>
             </div>
           </div>
-          <div className='user-content-filter'>
-            <button className='btn-filter'>Posts</button>
-            <button className='btn-filter btn-disabled'>Comments</button>
+          <div className="user-content-filter">
+            <button className="btn-filter">Posts</button>
+            <button className="btn-filter btn-disabled">Comments</button>
           </div>
-          <div className='posts-content-section'>
+          <div className="posts-content-section">
             {/* Fetch data from user/community posts here */}
+            {categorical_data.map((item) => {
+              return (
+                <>
+                  <h1>{item.title}</h1>
+                  <p>{item.text}</p>
+                  {item.thumnail && <img src={item.thumbnail} alt="thumbnail" />}
+                  {item.url && <p>{item.url}</p>}
+                  {item.is_video && item.videoLink}
+                </>
+              )
+            })}
           </div>
         </div>
       </div>
