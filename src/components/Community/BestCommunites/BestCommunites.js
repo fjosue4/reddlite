2eaeafe974a117085959525c6cf2dc7b34e9f49a
vikraj01 <99103720+vikraj01@users.noreply.github.com>
2022-12-07 02:11:38 +0530

    feat : Added more and hide functionality to best communities

--- src/components/Community/BestCommunites/BestCommunites.js
+++ src/components/Community/BestCommunites/BestCommunites.js
@@ -1,59 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './BestCommunites.css'
 import { Icon } from '@iconify/react'
 import { useSelector, useDispatch } from 'react-redux'
 import { useEffect } from 'react'
 import { getSubbreditsList } from '../../../store/subreddits/subredditSlice'
 
-
-const filterTopFiveCommunites = (data) => {
-  let topFive = []
-    for(let i = 0 ; i < data.length ; i++){
-      if(data[i].data.community_icon)
-        topFive.push(data[i])
-      if (topFive.length >= 5) break;
-    }
-
-
-    return topFive.map(item => {
-      return {
-        community_icon : item.data?.community_icon?.replace("&amp;","&"),
-        display_name : item.data?.display_name_prefixed
-      }
-    })
-}
-
-
 const BestCommunites = () => {
   const dispatch = useDispatch()
 
   const { data, error, loading } = useSelector((state) => state.subreddits)
+  const [more, setMore] = useState(false)
 
   useEffect(() => {
     dispatch(getSubbreditsList())
-  }, []);
+  }, [])
+  const filterTopFiveCommunites = (data) => {
+    let topFive = []
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].data.community_icon) topFive.push(data[i])
+      if (topFive.length >= 5 &&  !more) break
+    }
 
-
-  
+    return topFive.map((item) => {
+      return {
+        community_icon: item.data?.community_icon?.replace('&amp;', '&'),
+        display_name: item.data?.display_name_prefixed,
+      }
+    })
+  }
 
   const topFive = filterTopFiveCommunites(data)
 
   return (
     <div class="best-communities-container">
       <div class="best-communities-heading">Recommended communities</div>
-      {topFive.map((community, index) => (<div class="best-community-wrapper">
-        <div class="subreddit">
-          <span class="index">{index+1}</span>
-          <div class="best-community">
-            <div class="eclipse"><img src={community.community_icon}/></div>
-            <div class="thread">{community.display_name}</div>
+      {topFive.map((community, index) => (
+        <div class="best-community-wrapper">
+          <div class="subreddit">
+            <span class="index">{index + 1}</span>
+            <div class="best-community">
+              <div class="eclipse">
+                <img src={community.community_icon} />
+              </div>
+              <div class="thread">{community.display_name}</div>
+            </div>
           </div>
+          <button class="btn">
+            <Icon
+              className="right-arrow"
+              icon="ic:baseline-keyboard-arrow-right"
+            />
+          </button>
         </div>
-        <button class="btn"><Icon className='right-arrow' icon="ic:baseline-keyboard-arrow-right" /></button>
-      </div>)
-      )}
+      ))}
       <div class="button-wrapper">
-        <button class="btn">All</button>
+        <button onClick={() => setMore((prev) => !prev)} class="btn">
+          {more ? 'Hide' : 'More'}
+        </button>
       </div>
     </div>
   )
