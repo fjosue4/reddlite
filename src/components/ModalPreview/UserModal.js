619e2731d1f544845b2d50142092e11c2b6705dc
Franklin Méndez <franklinjosue.net@gmail.com>
2022-12-07 15:33:47 -0600

    Loading skeleton for user modal (Not responsive)

--- src/components/ModalPreview/UserModal.js
+++ src/components/ModalPreview/UserModal.js
@@ -7,7 +7,7 @@ import ReactImageFallback from 'react-image-fallback'
 
 const UserModal = () => {
   const dispatch = useDispatch()
-  const { info, data, icon } = useSelector((state) => state.modal)
+  const { info, data, icon, loading } = useSelector((state) => state.modal)
 
   console.log('INFO', info)
   console.log('DATA', data)
@@ -54,117 +54,167 @@ const UserModal = () => {
       return num
     }
   }
-
   const imgFormats = new RegExp(/\.(gif|jpe?g|tiff?|png|webp|bmp|gif)$/i)
 
   return (
     <div class="modal">
       <div class="modal-content">
-        <div className="modal-head">
-          <span class="close" onClick={modalHandler}>
-            <Icon icon="eva:arrow-ios-back-fill" />
-          </span>
-          <p className="url-preview">
-            /
-            {info?.kind === 't2'
-              ? `u/${info?.data.name}`
-              : info?.data.display_name_prefixed}
-          </p>
-        </div>
-        <div className="modal-body">
-          <div className="name-and-photo">
-            <ReactImageFallback
-              className="r-main-picture"
-              src={
-                icon
-                  ? icon
-                  : (info?.data.snoovatar_img?.trim() === ''
-                  ? info?.data.icon_img
-                  : info?.data.snoovatar_img)
-              }
-              fallbackImage="https://www.redditstatic.com/avatars/defaults/v2/avatar_default_4.png"
-            />
-            <div className="karma-or-members">
-              <h2>
-                {info?.kind === 't2'
-                  ? info?.data.name
-                  : info?.data.display_name}
-              </h2>
-              <span>
-                {info?.kind === 't2'
-                  ? numFormatter(
-                      info?.data.link_karma + info?.data.comment_karma,
-                    ) + ' Karma'
-                  : numFormatter(info?.data.subscribers) + ' Members'}
+        {loading ?
+          <>
+            <div className="modal-head">
+              <span class="close" onClick={modalHandler}>
+                <Icon icon="eva:arrow-ios-back-fill" />
               </span>
+              <p className="url-preview-skeleton">
+              </p>
             </div>
-          </div>
-          <div className="user-content-filter">
-            <button className="btn-filter">Posts</button>
-            <button className="btn-filter btn-disabled">Comments</button>
-          </div>
-          <div className="posts-content-section">
-            {/* Fetch data from user/community posts here */}
-            {categorical_data.map((item) => {
-              return (
-                <>
-                  {item.url?.toString().match(imgFormats) || item.thumnail ? (
-                    <div className="image-content">
-                      <div className="post-preview-two-col">
-                        <div className="post-preview-left">
-                          <h3>{item.title}</h3>
-                          {item.text && (
-                            <p className="p-preview">{item.text}</p>
-                          )}
-                        </div>
-                        <div className="photo-section">
-                          {item.thumnail && (
-                            <img src={item.thumbnail} alt="thumbnail" />
-                          )}
-                          <img src={item.url} alt="thumbnail" />
-                          {item.is_video && item.videoLink}
-                        </div>
+            <div className="modal-body">
+              <div className="name-and-photo">
+                <div className='icon-skeleton'></div>
+                <div className="karma-or-members-skeleton">
+                  <div className='name-skeleton'></div>
+                  <div className='second-info-skeleton'></div>
+                </div>
+              </div>
+              <div className="user-content-filter">
+                <button className="btn-filter">Posts</button>
+                <button className="btn-filter btn-disabled">Comments</button>
+              </div>
+              {[...Array(6)].map(() =>
+                <div className="posts-content-section">
+                  <div className="text-only">
+                    <div className="post-preview-left-skeleton">
+                      <p className="p-preview-skeleton"></p>
+                      <p className="p-preview-skeleton"></p>
+                      <p className="p-preview-skeleton"></p>
+                      <p className="p-preview-skeleton"></p>
+                    </div>
+                    <div className="post-preview-stats">
+                      <div className="comments-amount">
+                        <Icon
+                          className="stats-icon"
+                          icon="fluent:comment-24-regular"
+                        />{' '}
+                        Comments:
                       </div>
-                      <div className="post-preview-stats">
-                        <div className="comments-amount">
-                          <Icon icon="fluent:comment-24-regular" /> Comments:{' '}
-                          {item?.numComments}
-                        </div>
+                      <div className="upvote-ratio">
+                        <Icon
+                          className="stats-icon"
+                          icon="mdi:arrow-up-bold-outline"
+                        />{' '}% upvoted
                       </div>
                     </div>
-                  ) : (
-                    <div className="text-only">
-                      <div className="post-preview-left">
-                        <h3>{item.title}</h3>
-                        {item.text && <p className="p-preview">{item.text}</p>}
-                        <a href={item.url}>
-                          <p>{item.url}</p>
-                        </a>
-                        {item.is_video && item.videoLink}
-                      </div>
-                      <div className="post-preview-stats">
-                        <div className="comments-amount">
-                          <Icon
-                            className="stats-icon"
-                            icon="fluent:comment-24-regular"
-                          />{' '}
-                          Comments: {item?.numComments}
+                  </div>
+                </div>)}
+            </div>
+          </> :
+          <>
+            <div className="modal-head">
+              <span class="close" onClick={modalHandler}>
+                <Icon icon="eva:arrow-ios-back-fill" />
+              </span>
+              <p className="url-preview">
+                /
+                {info?.kind === 't2'
+                  ? `u/${info?.data.name}`
+                  : info?.data.display_name_prefixed}
+              </p>
+            </div>
+            <div className="modal-body">
+              <div className="name-and-photo">
+                <ReactImageFallback
+                  className="r-main-picture"
+                  src={
+                    icon
+                      ? icon
+                      : (info?.data.snoovatar_img?.trim() === ''
+                        ? info?.data.icon_img
+                        : info?.data.snoovatar_img)
+                  }
+                  fallbackImage="https://www.redditstatic.com/avatars/defaults/v2/avatar_default_4.png"
+                />
+                <div className="karma-or-members">
+                  <h2>
+                    {info?.kind === 't2'
+                      ? info?.data.name
+                      : info?.data.display_name}
+                  </h2>
+                  <span>
+                    {info?.kind === 't2'
+                      ? numFormatter(
+                        info?.data.link_karma + info?.data.comment_karma,
+                      ) + ' Karma'
+                      : numFormatter(info?.data.subscribers) + ' Members'}
+                  </span>
+                </div>
+              </div>
+              <div className="user-content-filter">
+                <button className="btn-filter">Posts</button>
+                <button className="btn-filter btn-disabled">Comments</button>
+              </div>
+              <div className="posts-content-section">
+                {/* Fetch data from user/community posts here */}
+                {categorical_data.map((item) => {
+                  return (
+                    <>
+                      {item.url?.toString().match(imgFormats) || item.thumnail ? (
+                        <div className="image-content">
+                          <div className="post-preview-two-col">
+                            <div className="post-preview-left">
+                              <h3>{item.title}</h3>
+                              {item.text && (
+                                <p className="p-preview">{item.text}</p>
+                              )}
+                            </div>
+                            <div className="photo-section">
+                              {item.thumnail && (
+                                <img src={item.thumbnail} alt="thumbnail" />
+                              )}
+                              <img src={item.url} alt="thumbnail" />
+                              {item.is_video && item.videoLink}
+                            </div>
+                          </div>
+                          <div className="post-preview-stats">
+                            <div className="comments-amount">
+                              <Icon icon="fluent:comment-24-regular" /> Comments:{' '}
+                              {item?.numComments}
+                            </div>
+                          </div>
                         </div>
-                        <div className="upvote-ratio">
-                          <Icon
-                            className="stats-icon"
-                            icon="mdi:arrow-up-bold-outline"
-                          />{' '}
-                          {item?.upVoteRatio}% upvoted
+                      ) : (
+                        <div className="text-only">
+                          <div className="post-preview-left">
+                            <h3>{item.title}</h3>
+                            {item.text && <p className="p-preview">{item.text}</p>}
+                            <a href={item.url}>
+                              <p>{item.url}</p>
+                            </a>
+                            {item.is_video && item.videoLink}
+                          </div>
+                          <div className="post-preview-stats">
+                            <div className="comments-amount">
+                              <Icon
+                                className="stats-icon"
+                                icon="fluent:comment-24-regular"
+                              />{' '}
+                              Comments: {item?.numComments}
+                            </div>
+                            <div className="upvote-ratio">
+                              <Icon
+                                className="stats-icon"
+                                icon="mdi:arrow-up-bold-outline"
+                              />{' '}
+                              {item?.upVoteRatio}% upvoted
+                            </div>
+                          </div>
                         </div>
-                      </div>
-                    </div>
-                  )}
-                </>
-              )
-            })}
-          </div>
-        </div>
+                      )}
+                    </>
+                  )
+                })}
+              </div>
+            </div>
+          </>}
       </div>
     </div>
   )
